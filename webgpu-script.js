c41a5acc29501ea90857d2acda1d433ac1890c84
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-07-27 07:48:37 +0800

    Clear pass

--- webgpu-script.js
+++ webgpu-script.js
@@ -33,4 +33,27 @@ if (!navigator.gpu) {
 
     // encoder records GPU commands
     const encoder = device.createCommandEncoder();
+
+    // drawing operations happens in render passes, let's begin a render pass
+    const pass = encoder.beginRenderPass({
+        // textures (called attachments) that receive output of drawing command
+        colorAttachments: [
+            {
+                // the part of texture to render to, this selects whole current canvas
+                view: ctx.getCurrentTexture().createView(),
+                // load operation to clear the texture
+                loadOp: "clear",
+                // parameter specific to clear operation
+                clearValue: { r: 0.3, g: 0.8, b: 1, a: 1 },
+                // store operation to 'store' the results of drawing into the texture
+                storeOp: "store",
+            },
+        ],
+    });
+
+    // End the render pass
+    pass.end();
+
+    // Finish the encoder and pass it to the GPU
+    device.queue.submit([encoder.finish()]);
 })();
